a8f19afd5427f1ef179f16facfa575fc5c3143e0
KanwarPadda <kanwarpadda1@gmail.com>
2021-11-17 22:06:27 -0500

    Initial commit

---
+++ app/src/main/java/ca/georgebrown/comp3074/project/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.georgebrown.comp3074.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
